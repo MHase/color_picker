9493ce64c74dc54a1032728f8cedc3edaf7bcb9b
Maciej <maciek.sykula@gmail.com>
2017-04-14 20:28:54 +0200

    added oapcity divs

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ var red, green, blue, opacity;
 document.addEventListener("DOMContentLoaded", function(event) {
   setRGB();
 
+  $('input[type="text"]').keyup(resizeInput).each(resizeInput);
 
-
-
-  // setRGB(red, green, blue, opacity);
-// hexString = yourNumber.toString(16);
   $('.preview').on('click', function() {
     var copy_color = document.querySelector('#rgb');
     copy_color.select();
@@ -21,10 +18,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
   });
 
 
-  $('input[type=range]').on('input', function() {
-    setRGB();
-  });
-
+  $('input[type=range]').on('input', function() { setRGB(); });
 
 });
 
@@ -39,23 +33,30 @@ function returnColor() {
 function setRGB() {
   returnColor();
   var color_rgba = "rgba(" + red + ", " + green + ", " + blue + ", " + opacity + ")";
-
   $('#rgb').val(color_rgba);
-  genHex();
-  var gradient = 'linear-gradient(to bottom,'
-  var rgba_gradient = "rgba(" + red + ", " + green + ", " + blue + ", ";
-  for (var i = 1; i >= 0; i = i-0.1) {
-    var temp = rgba_gradient;
-    temp += Math.round(i*10)/10 + ')';
-    (Math.round(i*10)/10 == 0) ? gradient += temp + ')' : gradient += temp + ', ';
-  }
-
+  $('#hex').val('#' + genHex(red, green, blue, opacity));
   $('.color').css('background', color_rgba);
-  // $('body').css('background', gradient);
+
+  addColorDivs(10);
 }
 
-function genHex() {
+function genHex(red, green, blue, opacity) {
   var temp = Math.round(Math.round(parseFloat(opacity)*100)/100*255);
-  var hex_text = '#' + temp.toString(16) + parseInt(red).toString(16) + parseInt(green).toString(16) + parseInt(blue).toString(16);
-  $('#hex').val(hex_text);
+  var hex_text = temp.toString(16) + parseInt(red).toString(16) + parseInt(green).toString(16) + parseInt(blue).toString(16);
+  return hex_text;
+}
+
+function resizeInput() {
+    $(this).attr('size', $(this).val().length);
+}
+
+function addColorDivs(step) {
+  $('.color_box').html("");
+  var tmp = 1/step;
+  var colors = [];
+  for (var i = 1; i > 0; i = i-tmp) {
+    var temp_opacity = Math.round(i*100)/100;
+    var background_rgba = 'rgba('+red+', '+green+','+blue+','+temp_opacity+')';
+    $('.color_box').append('<div class="color_div" style="background-color: ' + background_rgba +'">#'+genHex(red,green,blue,temp_opacity)+'</div>');
+  }
 }
